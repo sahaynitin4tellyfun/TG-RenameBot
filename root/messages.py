eb4f2bca82e597af7936c83d9aeac7622622111c
Animesh Verma <85546858+AVBotz-TG@users.noreply.github.com>
2021-07-25 23:30:05 +0530

    Update messages.py

--- root/messages.py
+++ root/messages.py
@@ -1,9 +1,32 @@
 class Translation(object):
-    START_TEXT = "**I'm a Rename and Convert Bot\nJust send me any media to change file name.\nUse /help command for more details **"
-######################
-    HELP_USER = """**>>Send File/Video\n>>Select desired Option\n>>And Done wait for it to process files**"""
-    DOWNLOAD_MSG = "**Downloading **⏬"
-    DOWNLOAD_FAIL_MSG = "**Failed to Download File**❎"
-    UPLOAD_MSG = "**Uploading** ⏫"
-    UPLOAD_FAIL_MSG = "**Failed to Upload File**❎"
-    UPLOAD_DONE_MSG = "**Uploaded Successfully 💡"
+    
+    START_TEXT = """
+**Hello {}, I'm a Simple File Renamer Bot with Permanent Thumbnail Support..! 💯
+
+Send any Telegram File or Video and Rename or Convert using me... 
+
+🤖 Developer : [Ravi Teja](https://t.me/MeRaviTeja)**
+➖➖➖➖➖➖➖➖➖➖➖➖
+__© Coded By [Animesh](https://t.me/Animesh941) ❤️__
+"""
+    HELP_USER = """
+**It's too simple to use me... Just follow these Steps..! 
+
+➠ Send any Image to set it as your Custom Thumbnail.
+➠ Send me any telegram file or media... 
+➠ Choose appropriate option...
+
+🗒️ Available Commands
+/start - To Start the Bot
+/help - To get this message
+/about - To get info about meh
+/forward - To forward a message to channel 
+/showthumb & /delthumb for Thumbnail**
+
+__© Coded By [Animesh](https://t.me/Animesh941)❤️__
+"""
+    DOWNLOAD_MSG = "**📥 Downloading....**"
+    DOWNLOAD_FAIL_MSG = "**🙄 Failed to download the given file..**"
+    UPLOAD_MSG = "📤 Now, Uploading....**"
+    UPLOAD_FAIL_MSG = "**😢 Failed to upload the renamed file..**"
+    UPLOAD_DONE_MSG = "**✅ Thanks for Using Meh...!**"
