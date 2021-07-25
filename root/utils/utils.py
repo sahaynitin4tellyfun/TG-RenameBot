125727d450c1e2a942dca18ab84d8d7a96844dda
Animesh Verma <85546858+AVBotz-TG@users.noreply.github.com>
2021-07-25 22:02:55 +0530

    Update utils.py

--- root/utils/utils.py
+++ root/utils/utils.py
@@ -34,7 +34,7 @@ async def progress_for_pyrogram(
         elapsed_time = TimeFormatter(milliseconds=elapsed_time)
         estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
 
-        progress = "[{0}{1}] \n\n**👀 Progress : {2}%\n".format(
+        progress = "**[{0}{1}] \n\n👀 Progress : {2}%\n\n**".format(
             ''.join(["●" for i in range(math.floor(percentage / 5))]),
             ''.join(["○" for i in range(20 - math.floor(percentage / 5))]),
             round(percentage, 2))
@@ -48,7 +48,7 @@ async def progress_for_pyrogram(
         )
         try:
             await message.edit(
-                text="{}\n {}**".format(
+                text="{}\n {}".format(
                     ud_type,
                     tmp
                 )
