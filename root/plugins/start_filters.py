be71b733f01612ae6755501c30a89b84e2b18174
Animesh Verma <85546858+AVBotz-TG@users.noreply.github.com>
2021-07-25 22:18:55 +0530

    Update start_filters.py

--- root/plugins/start_filters.py
+++ root/plugins/start_filters.py
@@ -18,31 +18,44 @@ from root.messages import Translation
 import pyrogram
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
+START_BUTTONS = InlineKeyboardMarkup(
+  [[
+    InlineKeyboardButton("🆘 Help", callback_data="help"),
+    InlineKeyboardButton("📝 About", callback_data="about"),
+    InlineKeyboardButton("🔐 Close", callback_data="close")
+   ]])
 
+HELP_BUTTONS = InlineKeyboardMarkup(
+  [[
+    InlineKeyboardButton("🏡 Home", callback_data="home"),
+    InlineKeyboardButton("📝 About", callback_data="about"),
+    InlineKeyboardButton("🔐 Close", callback_data="close")
+   ]])
+
+ABOUT_BUTTONS = InlineKeyboardMarkup(
+  [[
+    InlineKeyboardButton("😎 Developer 😎", url="https://t.me/Animesh941")
+   ],[
+    InlineKeyboardButton("🏡 Home", callback_data="home"),
+    InlineKeyboardButton("🔐 Close", callback_data="close")
+   ]])
 @Client.on_message(filters.command("help"))
 async def help_user(c,m):
-    try:
-       await m.reply_text(Translation.HELP_USER,quote=True)
-    except Exception as e:
-        log.info(str(e))
-        
+  await m.reply_text(Translation.HELP_USER, reply_markup=HELP_BUTTONS, disable_web_page_preview=True, quote=True)
+  
+@Client.on_message(filters.command("about"))
+async def about_user(c,m):
+  await m.reply_text(Translation.ABOUT_USER, reply_markup=ABOUT_BUTTONS, disable_web_page_preview=True, quote=True)
+  
 @Client.on_message(filters.command("start"))
 async def start_msg(c,m):
-    button = [[
-               InlineKeyboardButton("Owner ", url=f"https://t.me/{Config.OWNER_USERNAME}")
-             ]]
-    markup = InlineKeyboardMarkup(button) 
-    try:
-       await m.reply_text(Translation.START_TEXT,quote=True,reply_markup=markup,disable_web_page_preview=True) 
-    except Exception as e:
-        log.info(str(e))
-
-        
-@Client.on_message(filters.command("log") & filters.private & filters.user(Config.OWNER_ID))
+  await m.reply_text(Translation.START_TEXT, quote=True, reply_markup=START_BUTTONS, disable_web_page_preview=True)
+  
+@Client.on_message(filters.command("logs") & filters.private & filters.user(Config.OWNER_ID))
 async def log_msg(c,m):
-  z =await m.reply_text("Processing..", True)
+  z =await m.reply_text("**👀 Processing...**", True)
   if os.path.exists("Log.txt"):
      await m.reply_document("Log.txt", True)
      await z.delete()
   else:
-    await z.edit_text("Log file not found")
+    await z.edit_text("**👀 Log file not found..!**")
