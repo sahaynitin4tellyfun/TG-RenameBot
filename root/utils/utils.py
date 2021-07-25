524bd682e1f25f99c4aa5474e8f10c5a57b7eea5
Animesh Verma <85546858+AVBotz-TG@users.noreply.github.com>
2021-07-25 21:55:36 +0530

    Update utils.py

--- root/utils/utils.py
+++ root/utils/utils.py
@@ -34,12 +34,12 @@ async def progress_for_pyrogram(
         elapsed_time = TimeFormatter(milliseconds=elapsed_time)
         estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
 
-        progress = "[{0}{1}] \n<b>◌Progress😉</b>:<code>〘 {2}% 〙</code>\n".format(
+        progress = "[{0}{1}] \n\n**👀 Progress : {2}%\n".format(
             ''.join(["●" for i in range(math.floor(percentage / 5))]),
             ''.join(["○" for i in range(20 - math.floor(percentage / 5))]),
             round(percentage, 2))
         
-        tmp = progress + "<b>Done:</b> <code>〘{0}</code><b> of </b><code> {1}〙</code>\n<b>◌Speed🚀</b>:<code>〘 {2}/s 〙</code>\n<b>◌Time Left⏳</b>:<code>〘 {3} 〙</code>\n".format(
+        tmp = progress + "**✅ Total Done :{0} of {1}\n🔥 Speed : {2}/s \n🐌 Time Left : {3}\n\n__© Coded By Animesh Verma...! ❤️__**".format(
             humanbytes(current),
             humanbytes(total),
             humanbytes(speed),
