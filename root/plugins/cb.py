7080ba9ad50218959979748d1d391169c27c408f
Animesh Verma <85546858+AVBotz-TG@users.noreply.github.com>
2021-07-25 23:34:38 +0530

    Update cb.py

--- root/plugins/cb.py
+++ root/plugins/cb.py
@@ -63,7 +63,7 @@ async def renamer(c,m,as_file=False):
     extension = "mkv"
   await bot_msg.delete() # delete name asked msg 
   if len(new_f_name) > 64:
-      await m.reply_text(text=f"Limits of telegram file name is 64 charecters only\nReduce some and try again.")
+      await m.reply_text(text=f"**Limits of telegram file name is 64 charecters only\nReduce some and try again.**")
       return
   d_msg = await m.reply_text(Translation.DOWNLOAD_MSG,True)
   d_location = Config.DOWNLOAD_LOCATION + "/" + str(m.chat.id) + "/"
