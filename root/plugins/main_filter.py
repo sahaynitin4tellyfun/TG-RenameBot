e52ec10a0b4a292a9eea8bdbb3678ae4035d736b
Animesh Verma <85546858+AVBotz-TG@users.noreply.github.com>
2021-07-26 15:26:59 +0530

    Update main_filter.py

--- root/plugins/main_filter.py
+++ root/plugins/main_filter.py
@@ -12,7 +12,7 @@ logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 log = logging.getLogger(__name__)
 
-@Client.on_message(filters.document | filters.video | filters.audio | filters.voice | filters.video_note | filters.animation) 
+@Client.on_message(filters.private & (filters.document | filters.video | filters.audio | filters.voice | filters.video_note | filters.animation) & filters.user(Config.AUTH)) 
 async def rename_filter(c,m):
   media = m.document or m.video or m.audio or m.voice or m.video_note or m.animation
   ## couldn't add photo bcoz i want all photos to use as thumb..
@@ -21,23 +21,23 @@ async def rename_filter(c,m):
   button = []
   try:
     filename = media.file_name
-    text += f"FileName:\n{filename}\n"
+    text += f"**📂 File Name : {filename}**"
   except:
     # some files dont gib name ..
     filename = None 
     
-  text += "Select the desired Option"
-  button.append([InlineKeyboardButton("Rename as File", callback_data="rename_file")])
+  text += "**☑ Select the Desired Options from below buttons..!**"
+  button.append([InlineKeyboardButton("📂 Rename : File ", callback_data="rename_file")],[InlineKeyboardButton("🎞️ Rename : Video",callback_data="rename_video")])
   # Thanks to albert for mime_type suggestion 
   if media.mime_type.startswith("video/"):
     ## how the f the other formats can be uploaded as video 
-    button.append([InlineKeyboardButton("Rename as Video",callback_data="rename_video")])
-    button.append([InlineKeyboardButton("Convert as File",callback_data="convert_file")])
-    button.append([InlineKeyboardButton("Convert as Video",callback_data="convert_video")])
-  button.append([InlineKeyboardButton("Cancel ❌",callback_data="cancel")])
+    button.append([InlineKeyboardButton("🔁 Convert To File 📂",callback_data="convert_file")])
+    button.append([InlineKeyboardButton("🔁 Convert To Video 🎞️",callback_data="convert_video")])
+  button.append([InlineKeyboardButton("🗑️ Cancel Process... ❌",callback_data="cancel")])
  
   markup = InlineKeyboardMarkup(button)
   try:
-    await m.reply_text(text,quote=True,reply_markup=markup,parse_mode="markdown",disable_web_page_preview=True)
+    b = await m.reply_text("**__⚠️ Initiating Process...__**", True)
+    await b.edit_text(text, reply_markup=markup, parse_mode="markdown", disable_web_page_preview=True)
   except Exception as e:
     log.info(str(e))
