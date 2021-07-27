4ba08add927e242905ab486ae61070e954e594b1
Animesh Verma <85546858+AVBotz-TG@users.noreply.github.com>
2021-07-27 13:37:59 +0530

    Create database.py

---
+++ root/plugins/database.py
@@ -0,0 +1,27 @@
+import datetime
+import motor.motor_asyncio
+
+
+class Database:
+
+    def __init__(self, uri, database_name):
+        self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
+        self.db = self._client[database_name]
+        self.col = self.db.users
+
+    def new_user(self, id):
+        return dict(
+            id=id,
+            generate_ss=False,
+            generate_sample_video=False
+        )
+    async def get_generate_ss(self, id):
+        user = await self.col.find_one({'id': int(id)})
+        return user.get('generate_ss', False)
+
+    async def set_generate_sample_video(self, id, generate_sample_video):
+        await self.col.update_one({'id': id}, {'$set': {'generate_sample_video': generate_sample_video}})
+
+    async def get_generate_sample_video(self, id):
+        user = await self.col.find_one({'id': int(id)})
+        return user.get('generate_sample_video', False)
