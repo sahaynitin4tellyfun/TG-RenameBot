1e2569f0c532ece38882bc85456439cf10bb9e85
Animesh Verma <85546858+AVBotz-TG@users.noreply.github.com>
2021-07-27 14:30:29 +0530

    Update access_db.py

--- root/plugins/access_db.py
+++ root/plugins/access_db.py
@@ -2,4 +2,4 @@
 from root.config import Config
 from root.plugins.database import Database
 
-db = Database(Config.DB_URI, Config.SESSION_NAME)
+db = Database(Config.MONGODB_URI, Config.SESSION_NAME)
